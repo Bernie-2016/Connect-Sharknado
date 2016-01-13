00006be41c7ee224b7f407a9c2797ebdc2e12830
root <jafirmani@gmail.com>
2016-01-13 13:36:11 -0500

    article model

--- models/article.py
+++ models/article.py
@@ -8,50 +8,50 @@ from datetime import datetime
 
 import model
 
-class VideoProvider (model.Provider):
-    table_name = "video"
+class ArticleProvider (model.Provider):
+    table_name = "article"
 
     def create (self, record):
-        msg = "Inserting video record for '{0}'."
+        msg = "Inserting article record for '{0}'."
         logging.info(msg.format(record["title"].encode("utf8")))
         record["uuid"] = self.generate_uuid()
         record["timestamp_creation"] = datetime.now()
 
         with self.get_db_cursor() as cur:
-            cur.execute("INSERT INTO video (uuid, video_id, site, title, description, thumbnail_url, timestamp_creation, timestamp_publish, description_snippet) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (record["uuid"], record["video_id"], record["site"], record["title"], record["description"], record["thumbnail_url"], record["timestamp_creation"], record["timestamp_publish"], record["snippet"],))
-            return Video(record)
+            cur.execute("INSERT INTO article (uuid, article_id, site, title, description, thumbnail_url, timestamp_creation, timestamp_publish, description_snippet) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)", (record["uuid"], record["article_id"], record["site"], record["title"], record["description"], record["thumbnail_url"], record["timestamp_creation"], record["timestamp_publish"], record["snippet"],))
+            return Article(record)
 
     def exists (self, uuid):
         """ Returns True if record with uuid exists """
         return self.read (uuid) is not None
 
-    def exists_by_video_id (self, video_id):
-        """ Returns True if record with video_id exists """
-        return self.read_by_video_id (video_id) is not None
+    def exists_by_article_id (self, article_id):
+        """ Returns True if record with article_id exists """
+        return self.read_by_article_id (article_id) is not None
 
     def make_model (self, props):
-        return Video (props)
+        return Article (props)
 
     def read (self, uuid):
         with self.get_db_cursor() as cur:
-            cur.execute("SELECT * FROM video WHERE uuid = (%s)", (uuid,))
+            cur.execute("SELECT * FROM article WHERE uuid = (%s)", (uuid,))
             res = cur.fetchone()
 
             if res is not None:
-                return Video (res)
+                return Article (res)
             else:
                 return None
 
-    def read_by_video_id (self, video_id):
+    def read_by_article_id (self, article_id):
         with self.get_db_cursor() as cur:
-            cur.execute("SELECT * FROM video WHERE video_id = (%s)", (video_id,))
+            cur.execute("SELECT * FROM article WHERE article_id = (%s)", (article_id,))
             res = cur.fetchone()
 
             if res is not None:
-                return Video (res)
+                return Article (res)
             else:
                 return None
 
 
-class Video (model.Model):
-    object_type = 'video'
+class Article (model.Model):
+    object_type = 'article'
