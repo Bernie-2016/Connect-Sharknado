46003278ab00dfe66c89d7d39133a525e78281e6
root <jafirmani@gmail.com>
2016-01-13 13:42:06 -0500

    schema

---
+++ schema.sql
@@ -0,0 +1,68 @@
+CREATE TABLE video (
+    uuid uuid NOT NULL,
+    video_id text,
+    site text,
+    title text,
+    description text,
+    thumbnail_url text,
+    timestamp_creation timestamp with time zone,
+    timestamp_publish timestamp with time zone,
+    description_snippet text
+);
+
+CREATE UNIQUE INDEX index_video_uuid ON video (uuid);
+
+CREATE UNIQUE INDEX index_video_id ON video (video_id);
+
+
+CREATE TABLE event (
+    uuid uuid NOT NULL,
+    event_id text,
+    event_id_obfuscated text,
+    url text,
+    name text,
+    date text,
+    start_time timestamp without time zone,
+    timezone text,
+    description text,
+    latitude double precision,
+    longitude double precision,
+    attendee_count integer,
+    capacity integer,
+    site text,
+    lang text,
+    event_type_name text,
+    venue_address1 text,
+    venue_address2 text,
+    venue_address3 text,
+    venue_name text,
+    venue_city text,
+    venue_state text,
+    venue_zip integer,
+    timestamp_creation timestamp with time zone,
+    is_official boolean
+);
+
+CREATE UNIQUE INDEX index_event_uuid ON event (uuid);
+
+CREATE UNIQUE INDEX index_event_id ON event (event_id);
+
+
+CREATE TABLE issue (
+    uuid uuid NOT NULL,
+    url text,
+    site text,
+    lang text,
+    title text,
+    article_type text,
+    body text,
+    body_html text,
+    description text,
+    description_html text,
+    timestamp_creation timestamp with time zone,
+    timestamp_publish timestamp with time zone
+);
+
+CREATE UNIQUE INDEX index_issue_uuid ON video (uuid);
+
+CREATE UNIQUE INDEX index_issue_url ON video (url);
