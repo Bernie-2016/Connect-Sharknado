3201a273408f86cd350d9b4202d4792528e39317
sfscs <jacobneilgustafson@gmail.com>
2016-01-14 11:55:20 -0800

    push script js

--- static/movement.js
+++ static/movement.js
@@ -1,3 +1,10 @@
 $(document).ready( function() {
-	setTimeout("$('.status').fadeOut(1000)", 1000);
+	setTimeout("$('.status').fadeOut(1000)", 5000);
+	$("#push_notification").click(function(event) {
+		event.preventDefault();
+		if (confirm("Are you sure you want to send this push notification?") == true) {
+			$("input[name='_send_push'").val('1');
+			$("form.update").submit();
+		}
+	});
 });
